2fa0295481569cce564ff2812dc89b7cffba73c4
Shulammite-Aso <asoamarachi@gmail.com>
2021-05-08 23:24:17 +0100

    roundup start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -6,12 +6,12 @@ import AddLanguage from "./AddLanguage";
     return(
         <div className="card">
             <div className="text-center">
-            <h3 className="text-2xl">Add and Select Language</h3>
-            <p>Add languages which you know <mark>(Min 2)</mark> and <mark>(Max 4)</mark> and select which is your native.</p>
+            <h3 className="text-2xl mb-3">Add and Select Language</h3>
+            <p className="mb-3 text-gray-500">Add languages which you know <mark className="bg-mark border-solid border border-mkBorder">(Min 2)</mark> and <mark className="bg-mark border-solid border border-mkBorder">(Max 4)</mark> and select which is your native.</p>
             </div>
             <AddLanguage />
-            <div>
-            <button>Let's make list of vocabulary</button>
+            <div className="mt-5">
+            <button className="bg-primary text-white py-2 px-3.5 rounded">Let's make list of vocabulary</button>
             </div>
         </div>
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#4db538',
+      'mark': '#fcf8e3',
+      'mk-border': '#faebcc',
+     }),
+     borderColor: theme => ({
+      ...theme('colors'),
+       DEFAULT: theme('colors.gray.300', 'currentColor'),
+      'primary': '#3490dc',
+      'mkBorder': '#faebcc',
+      'danger': '#e3342f',
+     }),
     extend: {},
   },
   variants: {
